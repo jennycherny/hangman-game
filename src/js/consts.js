53ecf841008a267597840198e0840390ac87f54b
jennycherny <evchern.it@gmail.com>
2023-07-12 15:46:43 +0500

    Update consts.js

--- src/js/consts.js
+++ src/js/consts.js
@@ -1,3 +1,3 @@
-export const WORDS = ['code', 'frontend', 'programming']
+export const WORDS = ['parrot', 'hamster', 'mouse', 'crocodile', 'elephant', 'giraffe', 'leopard', 'monkey', 'tortoise', 'zebra', 'hendehog', 'boar', 'moose', 'bear', 'kangaroo', 'camel', 'shark', 'dolphin', 'giraffe']
 
 export const KEYBOARD_LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
