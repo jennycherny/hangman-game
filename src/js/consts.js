03c2058cdcbf7572079e9de02772cd326f579c89
jennycherny <evchern.it@gmail.com>
2023-07-17 12:46:32 +0500

    fix bags

---
+++ src/js/consts.js
@@ -0,0 +1,3 @@
+export const WORDS = ['parrot', 'hamster', 'mouse', 'crocodile', 'elephant', 'giraffe', 'leopard', 'monkey', 'tortoise', 'zebra', 'hendehog', 'boar', 'moose', 'bear', 'kangaroo', 'camel', 'shark', 'dolphin', 'giraffe']
+
+export const KEYBOARD_LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
